3e539569d65b7e8f9e7f2e2acee9e620091e88c3
mikuliku <rk20090425@qq.com>
2026-08-25 15:26:23 +0800

    Update RenderMaidJiuHu.java

--- src/main/java/com/mikuliku/touhoulittlemaidgtnh/client/render/RenderMaidJiuHu.java
+++ src/main/java/com/mikuliku/touhoulittlemaidgtnh/client/render/RenderMaidJiuHu.java
@@ -3,18 +3,28 @@ package com.mikuliku.touhoulittlemaidgtnh.client.render;
 import com.mikuliku.touhoulittlemaidgtnh.entity.EntityMaidJiuHu;
 import net.minecraft.client.model.ModelBiped;
 import net.minecraft.client.renderer.entity.RenderBiped;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.util.ResourceLocation;
 
-public class RenderMaidJiuHu extends RenderBiped<EntityMaidJiuHu> {
+public class RenderMaidJiuHu extends RenderBiped {
+
     private static final ResourceLocation TEXTURE =
-            new ResourceLocation("touhoulittlemaidgtnh", "textures/entity/maid_jiuhu.png");
+            new ResourceLocation(
+                    "touhoulittlemaidgtnh",
+                    "textures/entity/maid_jiuhu.png"
+            );
 
     public RenderMaidJiuHu() {
-        super(new ModelBiped(0.0F), 0.35F);
+        super(
+                new ModelBiped(0.0F),
+                0.35F
+        );
     }
 
     @Override
-    protected ResourceLocation getEntityTexture(EntityMaidJiuHu entity) {
+    protected ResourceLocation getEntityTexture(
+            EntityLiving entity
+    ) {
         return TEXTURE;
     }
 }
