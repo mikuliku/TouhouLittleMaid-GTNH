52c9a8a8936127d319e057cc29732642738521cc
mikuliku <rk20090425@qq.com>
2026-08-25 16:10:02 +0800

    Update EntityMaidJiuHu.java

--- src/main/java/com/mikuliku/touhoulittlemaidgtnh/entity/EntityMaidJiuHu.java
+++ src/main/java/com/mikuliku/touhoulittlemaidgtnh/entity/EntityMaidJiuHu.java
@@ -17,15 +17,9 @@ public class EntityMaidJiuHu extends EntityTameable {
 
     public EntityMaidJiuHu(World world) {
         super(world);
-
         this.setSize(0.6F, 1.5F);
-
         this.getNavigator().setAvoidsWater(true);
-
-        this.tasks.addTask(
-                0,
-                new EntityAISwimming(this)
-        );
+        this.tasks.addTask(0, new EntityAISwimming(this));
     }
 
     protected void applyEntityAttributes() {
@@ -49,57 +43,36 @@ public class EntityMaidJiuHu extends EntityTameable {
     }
 
     public boolean interact(EntityPlayer player) {
-
         if (!this.worldObj.isRemote
                 && this.isTamed()
                 && this.getOwner() == player) {
 
             player.addChatMessage(
                     new ChatComponentText(
-                            "§d酒狐§f：主人，需要我做些什么吗？"
-                    )
-            );
-
+                            "§d酒狐§f：主人，需要我做些什么吗？"));
             return true;
         }
 
         return super.interact(player);
     }
 
-    /**
-     * 设置酒狐的主人。
-     */
     public void setOwnerPlayer(EntityPlayer player) {
-
         this.setTamed(true);
 
         this.func_152115_b(
-                player.getUniqueID().toString()
-        );
+                player.getUniqueID().toString());
 
         this.tasks.addTask(
                 2,
                 new EntityAIFollowOwner(
-                        this,
-                        1.0D,
-                        4.0F,
-                        2.0F
-                )
-        );
+                        this, 1.0D, 4.0F, 2.0F));
     }
 
-    /**
-     * 酒狐的名称。
-     *
-     * Minecraft 1.7.10 的 EntityLiving 提供
-     * getCommandSenderName()。
-     */
     public String getCommandSenderName() {
         return "酒狐";
     }
 
     public void onLivingUpdate() {
-
         super.onLivingUpdate();
 
         if (chatCooldown > 0) {
@@ -115,18 +88,7 @@ public class EntityMaidJiuHu extends EntityTameable {
         chatCooldown = ticks;
     }
 
-    /**
-     * 向 AI 系统发送玩家消息。
-     */
-    public void askAI(
-            EntityPlayer player,
-            String message
-    ) {
-
-        MaidChatManager.request(
-                this,
-                player,
-                message
-        );
+    public void askAI(EntityPlayer player, String message) {
+        MaidChatManager.request(this, player, message);
     }
 }
