af3060744fca9cc39cdafc9de914a6baaab8e699
mikuliku <rk20090425@qq.com>
2026-08-25 16:01:09 +0800

    Update RenderMaidJiuHu.java

--- src/main/java/com/mikuliku/touhoulittlemaidgtnh/client/render/RenderMaidJiuHu.java
+++ src/main/java/com/mikuliku/touhoulittlemaidgtnh/client/render/RenderMaidJiuHu.java
@@ -1,30 +1,27 @@
 package com.mikuliku.touhoulittlemaidgtnh.client.render;
 
-import com.mikuliku.touhoulittlemaidgtnh.entity.EntityMaidJiuHu;
-import net.minecraft.client.model.ModelBiped;
+import com.mikuliku.touhoulittlemaidgtnh.client.model.ModelMaidJiuHu;
 import net.minecraft.client.renderer.entity.RenderBiped;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.util.ResourceLocation;
 
+/**
+ * 酒狐渲染器。
+ *
+ * 暂时使用 Minecraft 内置 Steve 纹理作为无额外二进制资源依赖的测试纹理。
+ * 下一阶段可直接替换为酒狐专用 PNG。
+ */
 public class RenderMaidJiuHu extends RenderBiped {
 
     private static final ResourceLocation TEXTURE =
-            new ResourceLocation(
-                    "touhoulittlemaidgtnh",
-                    "textures/entity/maid_jiuhu.png"
-            );
+            new ResourceLocation("minecraft", "textures/entity/steve/steve.png");
 
     public RenderMaidJiuHu() {
-        super(
-                new ModelBiped(0.0F),
-                0.35F
-        );
+        super(new ModelMaidJiuHu(), 0.35F);
     }
 
     @Override
-    protected ResourceLocation getEntityTexture(
-            EntityLiving entity
-    ) {
+    protected ResourceLocation getEntityTexture(EntityLiving entity) {
         return TEXTURE;
     }
 }
