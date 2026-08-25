d3537678119e986b115bdfef51a6525d93419191
mikuliku <rk20090425@qq.com>
2026-08-25 16:48:35 +0800

    Update RenderMaidJiuHu.java

--- src/main/java/com/mikuliku/touhoulittlemaidgtnh/client/render/RenderMaidJiuHu.java
+++ src/main/java/com/mikuliku/touhoulittlemaidgtnh/client/render/RenderMaidJiuHu.java
@@ -5,16 +5,10 @@ import net.minecraft.client.renderer.entity.RenderBiped;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.util.ResourceLocation;
 
-/**
- * 酒狐渲染器。
- *
- * 暂时使用 Minecraft 内置 Steve 纹理作为无额外二进制资源依赖的测试纹理。
- * 下一阶段可直接替换为酒狐专用 PNG。
- */
 public class RenderMaidJiuHu extends RenderBiped {
 
     private static final ResourceLocation TEXTURE =
-            new ResourceLocation("minecraft", "textures/entity/steve/steve.png");
+            new ResourceLocation("touhoulittlemaidgtnh", "textures/entity/jiuhu.png");
 
     public RenderMaidJiuHu() {
         super(new ModelMaidJiuHu(), 0.35F);
