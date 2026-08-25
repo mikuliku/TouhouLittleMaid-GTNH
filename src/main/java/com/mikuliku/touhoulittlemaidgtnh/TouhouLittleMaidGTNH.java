e1895e1af750612522f19e6388b0ef825da9eb00
mikuliku <rk20090425@qq.com>
2026-08-25 16:17:51 +0800

    Update TouhouLittleMaidGTNH.java

--- src/main/java/com/mikuliku/touhoulittlemaidgtnh/TouhouLittleMaidGTNH.java
+++ src/main/java/com/mikuliku/touhoulittlemaidgtnh/TouhouLittleMaidGTNH.java
@@ -5,6 +5,7 @@ import com.mikuliku.touhoulittlemaidgtnh.ai.MaidChatEventHandler;
 import com.mikuliku.touhoulittlemaidgtnh.ai.MaidMainThreadScheduler;
 import com.mikuliku.touhoulittlemaidgtnh.ai.ToolRegistry;
 import com.mikuliku.touhoulittlemaidgtnh.ai.tools.PlayerStatusTool;
+import com.mikuliku.touhoulittlemaidgtnh.ai.tools.RecipeSearchTool;
 import com.mikuliku.touhoulittlemaidgtnh.command.CommandJiuHu;
 import com.mikuliku.touhoulittlemaidgtnh.entity.EntityMaidJiuHu;
 import com.mikuliku.touhoulittlemaidgtnh.proxy.CommonProxy;
@@ -26,20 +27,27 @@ import net.minecraftforge.common.MinecraftForge;
 )
 public class TouhouLittleMaidGTNH {
 
-    public static final String MODID = "touhoulittlemaidgtnh";
-    public static final String VERSION = "0.3.0-gtnh";
+    public static final String MODID =
+            "touhoulittlemaidgtnh";
+
+    public static final String VERSION =
+            "0.4.0-gtnh";
 
     @SidedProxy(
-            clientSide = "com.mikuliku.touhoulittlemaidgtnh.proxy.ClientProxy",
-            serverSide = "com.mikuliku.touhoulittlemaidgtnh.proxy.CommonProxy"
+            clientSide =
+                    "com.mikuliku.touhoulittlemaidgtnh.proxy.ClientProxy",
+            serverSide =
+                    "com.mikuliku.touhoulittlemaidgtnh.proxy.CommonProxy"
     )
     public static CommonProxy proxy;
 
     @Mod.EventHandler
-    public void preInit(FMLPreInitializationEvent event) {
+    public void preInit(
+            FMLPreInitializationEvent event) {
 
         AIConfig.load(
-                event.getSuggestedConfigurationFile().getParentFile());
+                event.getSuggestedConfigurationFile()
+                        .getParentFile());
 
         EntityRegistry.registerModEntity(
                 EntityMaidJiuHu.class,
@@ -50,19 +58,28 @@ public class TouhouLittleMaidGTNH {
                 3,
                 true);
 
-        ToolRegistry.register(new PlayerStatusTool());
+        ToolRegistry.register(
+                new PlayerStatusTool());
+
+        ToolRegistry.register(
+                new RecipeSearchTool());
 
         MinecraftForge.EVENT_BUS.register(
                 new MaidChatEventHandler());
 
-        FMLCommonHandler.instance().bus().register(
-                new MaidMainThreadScheduler());
+        FMLCommonHandler.instance()
+                .bus()
+                .register(
+                        new MaidMainThreadScheduler());
 
         proxy.preInit();
     }
 
     @Mod.EventHandler
-    public void serverStarting(FMLServerStartingEvent event) {
-        event.registerServerCommand(new CommandJiuHu());
+    public void serverStarting(
+            FMLServerStartingEvent event) {
+
+        event.registerServerCommand(
+                new CommandJiuHu());
     }
 }
